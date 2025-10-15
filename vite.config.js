11bf13ccbdc2bf4d379d6f0cea3a52ca6ea34f65
ZeroFuann-FS <josejuanjuarezmartine14@gmail.com>
2025-10-15 16:04:59 -0600

    Update base path to /PaginaIA2/PaginaVideoJuegos/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PaginaIA2/',
+  base: '/PaginaIA2/PaginaVideoJuegos/',
   plugins: [vue()],
 })
